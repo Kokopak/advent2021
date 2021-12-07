178245158dab8f283182b30ed508e01bf7bd8354
Corentin Chataignon <corentin.chataignon44@gmail.com>
2021-12-07 19:52:19 +0100

    opti formula day7

--- day7/day7.py
+++ day7/day7.py
@@ -17,8 +17,15 @@ print(minim_fuel)
 # part 2
 minim_fuel = sys.maxsize
 
+
+def arithm_sum(n):
+    S = (n * (n + 1)) // 2
+
+    return S
+
+
 for i in range(max(crabs_pos)):
-    s = int(sum([((abs(x - i)) * (2 + (abs(x - i) - 1))) / 2 for x in crabs_pos]))
+    s = sum([arithm_sum(abs(x - i)) for x in crabs_pos])
 
     if s < minim_fuel:
         minim_fuel = s
