4b1b5130704290de052e98090dc1729dc231fab0
Corentin Chataignon <corentin.chataignon@kaduceo.com>
2021-12-09 16:18:23 +0100

    save

--- day8/day8.py
+++ day8/day8.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 patterns_outputs = []
-with open("input.txt") as f:
+with open("input2.txt") as f:
     for l in f.readlines():
         pattern_string, output_string = l.split(" | ")
 
@@ -42,41 +42,68 @@ contains = {
     9: [8],
 }
 
+inv_contains = {
+    0: [1, 7],
+    1: [],
+    2: [],
+    3: [1, 7],
+    4: [1],
+    5: [],
+    6: [5],
+    7: [1],
+    8: [0, 1, 2, 3, 4, 5, 6, 7, 9],
+    9: [1, 3, 4, 5, 7],
+}
+
 
 def get_decode(dcd, patterns):
     # print(dcd)
     nb_segments = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 
+    for k, v in dcd.items():
+        if v == 4:
+            FOUR = k
+
     if list(sorted(dcd.values())) == list(range(10)):
-        print("=>", dcd)
         return dcd
 
+    to_try = set()
     for pattern in patterns:
         if pattern not in dcd:
-
             set_pattern = set(pattern)
+            find = []
+
             for decode in dcd:
                 set_decode = set(decode)
-                if set_decode.issubset(set_pattern):
+
+                if set_decode.issubset(set_pattern) or set_pattern.issubset(set_decode):
                     for cont in contains[dcd[decode]]:
                         if (
                             cont not in dcd.values()
                             and len(pattern) == nb_segments[cont]
                         ):
-                            dcd_tmp = dcd.copy()
-                            dcd_tmp[pattern] = cont
-                            gd = get_decode(dcd_tmp, patterns)
+                            if cont == 0 or cont == 9:
+                                if set(FOUR).issubset(set_pattern):
+                                    to_try.add((pattern, 9))
+                                else:
+                                    to_try.add((pattern, 0))
+                            else:
+                                to_try.add((pattern, cont))
 
-                            if gd:
-                                return gd
+    if not len(to_try):
+        for pattern in patterns:
+            if pattern not in dcd:
+                for nb, nb_seg in enumerate(nb_segments):
+                    if nb_seg == len(pattern) and nb not in dcd.values():
+                        to_try.add((pattern, nb))
+    for tt in to_try:
+        p, nb = tt
+        dcd_tmp = dcd.copy()
+        dcd_tmp[p] = nb
+        gd = get_decode(dcd_tmp, patterns)
 
-            for nb, nb_segment in enumerate(nb_segments):
-                if nb_segment == len(pattern) and nb not in dcd.values():
-                    dcd_tmp = dcd.copy()
-                    dcd_tmp[pattern] = nb
-                    gd = get_decode(dcd_tmp, patterns)
-                    if gd:
-                        return gd
+        if gd:
+            return gd
 
 
 for po in patterns_outputs:
@@ -98,11 +125,10 @@ for po in patterns_outputs:
     )
     print(decoded)
 
-    # decoded_sorted = {"".join(sorted(k)): v for k, v in decoded.items()}
-    # decoded_output = []
-    # for output in outputs:
-    #     output_sorted = "".join(sorted(output))
-    #     decoded_output.append(decoded_sorted[output_sorted])
+    decoded_sorted = {"".join(sorted(k)): v for k, v in decoded.items()}
+    decoded_output = []
+    for output in outputs:
+        output_sorted = "".join(sorted(output))
+        decoded_output.append(decoded_sorted[output_sorted])
 
-    # print(decoded_output)
-    # # print([decoded[o] for o in output])
+    print(decoded_output)
