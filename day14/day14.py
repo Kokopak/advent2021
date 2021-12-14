7e238623fecca03c75be4d1101fa66d0e84ba5ee
Corentin Chataignon <corentin.chataignon@kaduceo.com>
2021-12-14 14:26:24 +0100

    clean day14

--- day14/day14.py
+++ day14/day14.py
@@ -9,25 +9,24 @@ rules = {r.split(" -> ")[0]: r.split(" -> ")[1] for r in rules.split("\n")}
 pair_counter = defaultdict(int)
 letter_counter = defaultdict(int)
 
-for i in range(len(polymer) - 1):
-    pair_counter[polymer[i : i + 2]] += 1
+for i, m in enumerate(polymer):
+    if i < len(polymer) - 1:
+        pair_counter[polymer[i : i + 2]] += 1
 
-for m in polymer:
     letter_counter[m] += 1
 
 for i in range(40):
     old_pair_counter = pair_counter.copy()
+
     for pair in old_pair_counter:
-
-        if old_pair_counter[pair] > 0:
-            mol, next_mol = list(pair)
-
-            pair_counter[f"{mol}{rules[pair]}"] += old_pair_counter[pair]
-            pair_counter[f"{rules[pair]}{next_mol}"] += old_pair_counter[pair]
-
-            letter_counter[rules[pair]] += old_pair_counter[pair]
-
         pair_counter[pair] -= old_pair_counter[pair]
+
+        mol, next_mol = list(pair)
+
+        pair_counter[f"{mol}{rules[pair]}"] += old_pair_counter[pair]
+        pair_counter[f"{rules[pair]}{next_mol}"] += old_pair_counter[pair]
+
+        letter_counter[rules[pair]] += old_pair_counter[pair]
 
     # p1
     if i == 9:
