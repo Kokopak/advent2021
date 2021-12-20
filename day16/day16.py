ef726c11ed93cb26a65546ff2b7c61310d3f4cbd
Corentin Chataignon <corentin.chataignon44@gmail.com>
2021-12-20 01:40:35 +0100

    day16

--- day16/day16.py
+++ day16/day16.py
@@ -1,6 +1,5 @@
 import functools
 
-
 HEX_TO_BIN = {
     "0": "0000",
     "1": "0001",
@@ -45,64 +44,27 @@ def pop(bin_str, index):
 S = 0
 
 
-def gen_packet_p1(bin_str):
+def gen_packet(bin_str):
     global S
-    if bin_str == [] or len(set(bin_str)) == 1:
-        return
-
-    version = int("".join(pop(bin_str, 3)), 2)
-    type_id = int("".join(pop(bin_str, 3)), 2)
-
-    S += version
-
-    if type_id == 4:
-        print(f"LV {version} +>", "".join(bin_str))
-        groups = [
-            "".join(pop(bin_str, 5))
-            for _ in range(0, len(bin_str), 5)
-            if bin_str[0] != "0"
-        ]
-        groups.append("".join(pop(bin_str, 5)))
-        gen_packet_p1(bin_str)
-    else:
-        print(f"OP {version} {type_id} +>", "".join(bin_str))
-
-        length_type_id = int("".join(pop(bin_str, 1)), 2)
-
-        if length_type_id == 0:
-            length_sub_packet = int("".join(pop(bin_str, 15)), 2)
-            gen_packet_p1(pop(bin_str, length_sub_packet))
-        else:
-            number_sub_packets = int("".join(pop(bin_str, 11)), 2)
-            for _ in range(number_sub_packets):
-                gen_packet_p1(bin_str)
-
-    gen_packet_p1(bin_str)
-
-
-def gen_packet_p2(bin_str):
 
     if isinstance(bin_str, int):
         return bin_str
 
-    if bin_str == [] or len(set(bin_str)) == 1:
-        return 0
-
     version = int("".join(pop(bin_str, 3)), 2)
+    S += version
+
     type_id = int("".join(pop(bin_str, 3)), 2)
 
     if type_id == 4:
-        print(f"LV {version} +>", "".join(bin_str))
         groups = [
-            "".join(pop(bin_str, 5))
+            "".join(pop(bin_str, 5))[1:5]
             for _ in range(0, len(bin_str), 5)
             if bin_str[0] != "0"
         ]
-        groups.append("".join(pop(bin_str, 5)))
+        groups.append("".join(pop(bin_str, 5))[1:5])
 
-        return gen_packet_p2(int("".join(groups), 2))
+        return int("".join(groups), 2)
     else:
-        print(f"OP {version} {type_id} +>", "".join(bin_str))
         packets = []
 
         length_type_id = int("".join(pop(bin_str, 1)), 2)
@@ -110,24 +72,20 @@ def gen_packet_p2(bin_str):
         if length_type_id == 0:
             length_sub_packet = int("".join(pop(bin_str, 15)), 2)
             packet = pop(bin_str, length_sub_packet)
-            while packet != "":
-                gp2 = gen_packet_p2(packet)
-                packets.append(gp2)
+
+            while packet != []:
+                packets.append(gen_packet(packet))
         else:
             number_sub_packets = int("".join(pop(bin_str, 11)), 2)
+
             for _ in range(number_sub_packets):
-                gp2 = gen_packet_p2(bin_str)
-                packets.append(gp2)
+                packets.append(gen_packet(bin_str))
 
-        print(type_id, packets)
-        if len(packets):
-            # print(type_id, packets, functools.reduce(TYPE_ID_OP[type_id], packets))
-            return functools.reduce(TYPE_ID_OP[type_id], packets)
-
-    # return gen_packet_p2(bin_str)
+        return functools.reduce(TYPE_ID_OP[type_id], packets)
 
 
-bin_str = list(hex_to_bin("9C0141080250320F1802104A08"))
-# gen_packet_p1(bin_str)
-# print(S)
-gen_packet_p2(bin_str)
+with open("input.txt") as f:
+    bin_str = list(hex_to_bin(f.read()))
+
+print(gen_packet(bin_str))
+print(S)
